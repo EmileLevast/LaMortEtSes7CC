0a071d4c1236528388f1daa1770a68fae7f36e65
emile <emilelevast@outlook.fr>
2024-06-26 18:24:46 +0200

    [CHORE] Initialisation sans erreur de compilation du serveur

--- server/src/main/kotlin/org/levast/project/Application.kt
+++ server/src/main/kotlin/org/levast/project/Application.kt
@@ -4,9 +4,16 @@ import Greeting
 import SERVER_PORT
 import ch.qos.logback.classic.Level
 import ch.qos.logback.classic.LoggerContext
+import io.ktor.http.HttpMethod
+import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import io.ktor.server.plugins.callloging.CallLogging
+import io.ktor.server.plugins.compression.Compression
+import io.ktor.server.plugins.compression.gzip
+import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.server.plugins.cors.routing.CORS
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.util.logging.KtorSimpleLogger
