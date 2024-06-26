b86475051412b056982dd6e7434c6699799802dd
emile <emilelevast@outlook.fr>
2024-06-26 18:12:20 +0200

    [CHORE] ajout de l'initialisation du serveur initial

--- server/src/main/kotlin/org/levast/project/Application.kt
+++ server/src/main/kotlin/org/levast/project/Application.kt
@@ -2,18 +2,43 @@ package org.levast.project
 
 import Greeting
 import SERVER_PORT
+import ch.qos.logback.classic.Level
+import ch.qos.logback.classic.LoggerContext
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.ktor.util.logging.KtorSimpleLogger
+import org.slf4j.LoggerFactory
 
 fun main() {
+
+    val logger = KtorSimpleLogger("logger")
+
+    (LoggerFactory.getILoggerFactory() as LoggerContext).getLogger("org.mongodb.driver").level = Level.WARN
+
+
     embeddedServer(Netty, port = SERVER_PORT, host = "0.0.0.0", module = Application::module)
         .start(wait = true)
 }
 
 fun Application.module() {
+    install(ContentNegotiation) {
+        json()
+    }
+    install(CallLogging) {
+        level = org.slf4j.event.Level.INFO
+    }
+    install(CORS) {
+        allowMethod(HttpMethod.Get)
+        allowMethod(HttpMethod.Post)
+        allowMethod(HttpMethod.Delete)
+        anyHost()
+    }
+    install(Compression) {
+        gzip()
+    }
     routing {
         get("/") {
             call.respondText("Ktor: ${Greeting().greet()}")
