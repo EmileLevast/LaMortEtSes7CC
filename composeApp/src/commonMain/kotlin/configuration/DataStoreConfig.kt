35fb05c2c429b4ec7719b8b16afc182866cdb0d7
emile <emilelevast@outlook.fr>
2024-07-12 13:12:05 +0200

    [FIX] ecrit correctement dans les sharedPref

--- composeApp/src/commonMain/kotlin/configuration/DataStoreConfig.kt
+++ composeApp/src/commonMain/kotlin/configuration/DataStoreConfig.kt
@@ -12,18 +12,28 @@ import okio.Path.Companion.toPath
 /**
  * Gets the singleton DataStore instance, creating it if necessary.
  */
-private fun getDataStore(): DataStore<Preferences> =
-    PreferenceDataStoreFactory.createWithPath(
-        produceFile = { dataStoreFileName.toPath() }
-    )
+private fun getDataStore(): DataStore<Preferences> {
+    if (dataStore == null) {
+        dataStore = PreferenceDataStoreFactory.createWithPath(
+            produceFile = { dataStoreFileName.toPath() }
+        )
+    }
+    return dataStore as DataStore<Preferences>
+}
 
-suspend fun getUrlSharedPreferences() = getDataStore().data.map { preferences -> preferences[PREF_KEY_URL_SERVER] ?: "localhost"}.first()
+suspend fun getUrlSharedPreferences(): String {
+    val urlFoundDataStore = getDataStore().data.map { preferences ->
+        preferences[PREF_KEY_URL_SERVER] ?: "localhost"
+    }
+    return urlFoundDataStore.first()
+}
 
-suspend fun setNewIpAdressToPreferences(newUrl:String){
+suspend fun setNewIpAdressToPreferences(newUrl: String) {
     getDataStore().edit { preferences ->
         preferences[PREF_KEY_URL_SERVER] = newUrl
     }
 }
 
+private var dataStore:DataStore<Preferences>?=null
 private const val dataStoreFileName = "datastore.preferences_pb"
 private val PREF_KEY_URL_SERVER = stringPreferencesKey("PREF_KEY_URL_SERVER")
