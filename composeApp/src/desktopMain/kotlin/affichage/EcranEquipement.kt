f46c223108bc4924a154e7e77f7cc4af903f0352
emile <emilelevast@outlook.fr>
2024-07-30 22:52:33 +0200

    [FEAT] sticky header

--- composeApp/src/desktopMain/kotlin/affichage/EcranEquipement.kt
+++ composeApp/src/desktopMain/kotlin/affichage/EcranEquipement.kt
@@ -1,8 +1,10 @@
 package affichage
 
 import IListItem
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.draggable
@@ -15,6 +17,7 @@ import androidx.compose.foundation.lazy.grid.LazyGridScope
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
@@ -26,6 +29,7 @@ import androidx.compose.material.icons.filled.Star
 import androidx.compose.material.icons.twotone.Star
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
@@ -68,130 +72,137 @@ fun layoutListItem(
 
     val groupsEquipements = equipementsAfficher.groupBy { it::class.simpleName }
 
+    Box(Modifier.then(modifier)){
+        LazyVerticalGrid(
+            state = scrollState,
+            columns = GridCells.Adaptive(minSize = graphicsConsts.cellMinWidth),
+            verticalArrangement = Arrangement.spacedBy(graphicsConsts.cellSpace),
+            horizontalArrangement = Arrangement.spacedBy(graphicsConsts.cellSpace),
+            modifier = Modifier.draggable(
+                orientation = Orientation.Vertical,
+                state = rememberDraggableState { delta ->
+                    coroutineScope.launch {
+                        scrollState.scrollBy(-delta)
+                    }
+                },
+            )
+        ) {
 
-    LazyVerticalGrid(
-        state = scrollState,
-        columns = GridCells.Adaptive(minSize = graphicsConsts.cellMinWidth),
-        verticalArrangement = Arrangement.spacedBy(graphicsConsts.cellSpace),
-        horizontalArrangement = Arrangement.spacedBy(graphicsConsts.cellSpace),
-        modifier = Modifier.draggable(
-            orientation = Orientation.Vertical,
-            state = rememberDraggableState { delta ->
-                coroutineScope.launch {
-                    scrollState.scrollBy(-delta)
-                }
-            },
-        ).then(modifier)
-    ) {
+            groupsEquipements.forEach { (groupName, equipementsGrouped) ->
+                header(groupName?:"Xaraxatrailles", graphicsConsts.colorBackgroundSmallHeader, graphicsConsts.colorSmallHeader)
 
-        groupsEquipements.forEach { (groupName, equipementsGrouped) ->
-            header(groupName?:"Xaraxatrailles", graphicsConsts.colorBackgroundSmallHeader, graphicsConsts.colorSmallHeader)
+                items(equipementsGrouped) { equipement ->
+                    Card(
+                        modifier = Modifier.fillMaxHeight().clickable { showBigElement(equipement) },
+                        backgroundColor = equipement.color,
+                        elevation = graphicsConsts.cardElevation
+                    ) {
 
-            items(equipementsGrouped) { equipement ->
-                Card(
-                    modifier = Modifier.fillMaxHeight().clickable { showBigElement(equipement) },
-                    backgroundColor = equipement.color,
-                    elevation = graphicsConsts.cardElevation
-                ) {
-
-                    Box {
+                        Box {
 // Get local density from composable
-                        val localDensity = LocalDensity.current
-                        // Create element height in dp state
-                        var columnHeightDp by remember {
-                            mutableStateOf(0.dp)
-                        }
+                            val localDensity = LocalDensity.current
+                            // Create element height in dp state
+                            var columnHeightDp by remember {
+                                mutableStateOf(0.dp)
+                            }
 
 
-                        //Afficher le bouton pin si on est en mode Admin
-                        if(isModeAdmin && listPinnedItems!=null){
-                            if(listPinnedItems.contains(equipement._id)){
-                                IconButton(onClick = {
-                                    togglePinItem(equipement._id,false)
-                                })
-                                {
-                                    Icon(Icons.Filled.Star, "Desinpegler")
+                            //Afficher le bouton pin si on est en mode Admin
+                            if(isModeAdmin && listPinnedItems!=null){
+                                if(listPinnedItems.contains(equipement._id)){
+                                    IconButton(onClick = {
+                                        togglePinItem(equipement._id,false)
+                                    })
+                                    {
+                                        Icon(Icons.Filled.Star, "Desinpegler")
+                                    }
+                                }else{
+                                    IconButton(onClick = {
+                                        togglePinItem(equipement._id,true)
+                                    })
+                                    {
+                                        Icon(Icons.TwoTone.Star, "Epingler")
+                                    }
                                 }
-                            }else{
-                                IconButton(onClick = {
-                                    togglePinItem(equipement._id,true)
-                                })
-                                {
-                                    Icon(Icons.TwoTone.Star, "Epingler")
+                            }
+
+                            //Si on dipose d'une image de fond et que le mode détails n'est pas activé (le mode détail n'affiche pas les images)
+                            if(!isDetailedModeOn){
+                                if (imageBackground != null) {
+                                    Image(
+                                        modifier = Modifier.onGloballyPositioned { coordinates ->
+                                            // Set column height using the LayoutCoordinates
+                                            columnHeightDp = with(localDensity) { coordinates.size.height.toDp() }
+                                        },
+                                        painter = CustomPainterCard(imageBackground, equipement.getImage(apiApp) ?: imageResource(Res.drawable.UnknownImage)),
+                                        contentDescription = null,
+                                    )
+                                }
+                                else {
+                                    Image(
+                                        modifier = Modifier.onGloballyPositioned { coordinates ->
+                                            // Set column height using the LayoutCoordinates
+                                            columnHeightDp = with(localDensity) { coordinates.size.height.toDp() }
+                                        },
+                                        painter = painterResource("UnknownImage.jpg"),
+                                        contentDescription = null,
+                                    )
                                 }
                             }
-                        }
-
-                        //Si on dipose d'une image de fond et que le mode détails n'est pas activé (le mode détail n'affiche pas les images)
-                        if(!isDetailedModeOn){
-                            if (imageBackground != null) {
-                                Image(
-                                    modifier = Modifier.onGloballyPositioned { coordinates ->
-                                        // Set column height using the LayoutCoordinates
-                                        columnHeightDp = with(localDensity) { coordinates.size.height.toDp() }
-                                    },
-                                    painter = CustomPainterCard(imageBackground, equipement.getImage(apiApp) ?: imageResource(Res.drawable.UnknownImage)),
-                                    contentDescription = null,
-                                )
-                            }
-                            else {
-                                Image(
-                                    modifier = Modifier.onGloballyPositioned { coordinates ->
-                                        // Set column height using the LayoutCoordinates
-                                        columnHeightDp = with(localDensity) { coordinates.size.height.toDp() }
-                                    },
-                                    painter = painterResource("UnknownImage.jpg"),
-                                    contentDescription = null,
-                                )
-                            }
-                        }
 
 
-                        Column(
-                            modifier = if (!isDetailedModeOn) {
-                                Modifier.padding(graphicsConsts.cellContentPadding)
-                                    .height(columnHeightDp - (graphicsConsts.cellContentPadding * 2))
-                            } else {
-                                Modifier
-                            }
-                        ) {
-                            if(isDetailedModeOn){
-                                Text(
-                                    modifier = Modifier.fillMaxWidth(),
-                                    text = equipement.nomComplet.ifBlank { equipement.nom },
-                                    textAlign = TextAlign.Center,
-                                    style = MaterialTheme.typography.h5,
-                                    fontFamily = FontFamily(Font(graphicsConsts.fontCard)),
-                                    color = Color.Black
-                                )
-                                Text(
-                                    modifier = Modifier.fillMaxWidth(),
-                                    text = equipement.getStatsAsStrings(),
-                                    textAlign = TextAlign.Left,
-                                    style = MaterialTheme.typography.body1,
-                                    color = Color.Black
-                                )
-                            }else{
-                                Text(
-                                    modifier = Modifier.fillMaxWidth(),
-                                    text = equipement.nomComplet.ifBlank { equipement.nom },
-                                    textAlign = TextAlign.Center,
-                                    style = MaterialTheme.typography.h5,
-                                    fontFamily = FontFamily(Font(graphicsConsts.fontCard)),
-                                    color = Color.White
-                                )
+                            Column(
+                                modifier = if (!isDetailedModeOn) {
+                                    Modifier.padding(graphicsConsts.cellContentPadding)
+                                        .height(columnHeightDp - (graphicsConsts.cellContentPadding * 2))
+                                } else {
+                                    Modifier
+                                }
+                            ) {
+                                if(isDetailedModeOn){
+                                    Text(
+                                        modifier = Modifier.fillMaxWidth(),
+                                        text = equipement.nomComplet.ifBlank { equipement.nom },
+                                        textAlign = TextAlign.Center,
+                                        style = MaterialTheme.typography.h5,
+                                        fontFamily = FontFamily(Font(graphicsConsts.fontCard)),
+                                        color = Color.Black
+                                    )
+                                    Text(
+                                        modifier = Modifier.fillMaxWidth(),
+                                        text = equipement.getStatsAsStrings(),
+                                        textAlign = TextAlign.Left,
+                                        style = MaterialTheme.typography.body1,
+                                        color = Color.Black
+                                    )
+                                }else{
+                                    Text(
+                                        modifier = Modifier.fillMaxWidth(),
+                                        text = equipement.nomComplet.ifBlank { equipement.nom },
+                                        textAlign = TextAlign.Center,
+                                        style = MaterialTheme.typography.h5,
+                                        fontFamily = FontFamily(Font(graphicsConsts.fontCard)),
+                                        color = Color.White
+                                    )
+                                }
                             }
                         }
                     }
-                }
 
+                }
             }
         }
-
-
-
-
+        if(groupsEquipements.isNotEmpty()){
+            Surface (modifier = Modifier.clip(RoundedCornerShape(10.dp)).fillMaxWidth()){
+                Text(groupsEquipements.keys.first()?:"Vide", color = graphicsConsts.colorSmallHeader,
+                    textAlign = TextAlign.Center, modifier = Modifier.background(graphicsConsts.colorBackgroundSmallHeader).fillMaxWidth())
+            }
+        }
     }
+
+
+
+
 
     if(equipementToShow!=null && !isDetailedModeOn){
         layoutBigImage(equipementToShow, hideBigElement, isShowingStats)
@@ -206,6 +217,8 @@ fun LazyGridScope.header(
 
 
     item(span = { GridItemSpan(this.maxLineSpan) }, content = {
-            Text(title, modifier = Modifier.background(colorBackground), color = colorFront, textAlign = TextAlign.Center,)
+        Surface (modifier = Modifier.clip(RoundedCornerShape(10.dp))){
+            Text(title, color = colorFront, textAlign = TextAlign.Center, modifier = Modifier.background(colorBackground))
+        }
     })
 }
