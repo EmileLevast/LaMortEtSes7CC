ac7f4182eac0e326d1caf4f0429f0a1ef108606f
emile <emilelevast@outlook.fr>
2024-09-29 19:08:01 +0200

    Ajout d'un bouton plus pour les détails

--- composeApp/src/desktopMain/kotlin/affichage/EcranDetailJoueur.kt
+++ composeApp/src/desktopMain/kotlin/affichage/EcranDetailJoueur.kt
@@ -2,12 +2,17 @@ package affichage
 
 import Joueur
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -21,11 +26,16 @@ fun layoutDetailJoueur(actuelJoueur:Joueur){
 
     val graphicsConsts = koinInject<GraphicConstantsFullGrid>()
 
-    Column {
+    Column(Modifier.fillMaxWidth()) {
         actuelJoueur.details.split("\n").forEach {
-            Card(Modifier.fillMaxWidth().padding(3.dp),border = BorderStroke(graphicsConsts.widthBorder, graphicsConsts.brushSpecialBorder), elevation = graphicsConsts.cardElevation) {
-                Text(it, Modifier.fillMaxWidth().padding(5.dp), textAlign = TextAlign.Center)
+            if(it.isNotBlank()){
+                Card(Modifier.fillMaxWidth().padding(3.dp),border = BorderStroke(graphicsConsts.widthBorder, graphicsConsts.brushSpecialBorder), elevation = graphicsConsts.cardElevation) {
+                    Text(it, Modifier.fillMaxWidth().padding(5.dp), textAlign = TextAlign.Center)
+                }
             }
+        }
+        Card(Modifier.fillMaxWidth(0.4f).align(Alignment.CenterHorizontally).padding(3.dp),border = BorderStroke(graphicsConsts.widthBorder, Color.LightGray), elevation = graphicsConsts.cardElevation, shape = RoundedCornerShape(50)) {
+            Icon(Icons.Rounded.Add, "ajouter detail", tint = Color.LightGray)
         }
     }
 }
